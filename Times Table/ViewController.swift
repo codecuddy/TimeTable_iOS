364503fdc88de2948ef0b16c5c9da10d9227128a
Charlie <charliecuddy4@gmail.com>
2017-11-29 23:25:38 -0600

    use slider to list first 50 multiples of a number from 1 to 20

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sliderValue: UILabel!
+    @IBOutlet weak var table: UITableView!
+
+    @IBAction func sliderUpdated(_ sender: Any) {
+        sliderValue.text = String(Int(slider.value * 20))
+        table.reloadData()
+    }
+    
+
+    internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 50
+    }
+    
+    internal func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = String(Int(slider.value * 20)) + " x " + String(indexPath.row + 1) + " = " + String((Int(slider.value * 20)) * (indexPath.row + 1))
+        return cell
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
